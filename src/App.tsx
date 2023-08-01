99b471858a779ef5c4f749f57a3187932bbdb5dd
Roksolana Tylipska <roksolana1407@gmail.com>
2023-08-01 22:56:48 +0300

    page-not-found

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import './App.scss';
 import { Footer } from './components/Footer';
+import { NotFoundPage } from './Pages/NotFoundPage';
 
-export const App: React.FC = () => <Footer />;
+export const App: React.FC = () => (
+  <>
+    <NotFoundPage />
+
+    <Footer />
+  </>
+);
