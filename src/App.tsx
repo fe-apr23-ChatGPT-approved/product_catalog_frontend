e1f16b1d4ab743c3b8a04359ebe160d82fa2b687
Vadym Vitruk <vadym_v@ukr.net>
2023-08-01 16:28:46 +0300

    mock_load_data_from_server

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,24 @@
-import React from 'react';
 import './App.scss';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Phone } from './types/Phone';
 
-export const App: React.FC = () => <h1>Hello!</h1>;
+export const App: React.FC = () => {
+  const [products, setProducts] = useState<Phone[]>([]);
+
+  const URL = 'http://localhost:3000/api/phones.json';
+
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then(response => {
+        setProducts(response.data);
+      });
+  }, []);
+
+  console.log(products);
+
+  return (
+    <h1>Hello!</h1>
+  );
+};
---
+++ src/types/Phone.ts
@@ -0,0 +1,14 @@
+export interface Phone {
+    id: string;
+    phoneId: string;
+    itemId: string;
+    name: string;
+    fullPrice: number;
+    price: number;
+    screen: string;
+    capacity: string;
+    color: string;
+    ram: string;
+    year: number;
+    image: string;
+}
