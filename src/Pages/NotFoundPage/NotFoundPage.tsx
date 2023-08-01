3f33debddbbde221c993faacaae3e63b745dcef0
Roksolana Tylipska <roksolana1407@gmail.com>
2023-08-01 23:42:58 +0300

    page-not-found

--- src/Pages/NotFoundPage/NotFoundPage.tsx
+++ src/Pages/NotFoundPage/NotFoundPage.tsx
@@ -4,20 +4,22 @@ import astronaut from '../../images/page-not-found-astronaut.png';
 
 export const NotFoundPage: React.FC = () => (
   <div className={style.not_found_page}>
-    
     <h1 className={style.not_found_page__title}>404</h1>
-    
+
     <h3 className={style.not_found_page__text}>
       LOST IN
       <span className={style.not_found_page__text__crossedOut}> SPACE </span>
       App-Name? Hmm, looks like this page is not found.
     </h3>
-    
+
     <a href="#">
       <button className={style.not_found_page__link}>Go Home</button>
     </a>
 
-    <img className={style.not_found_page__img} src={astronaut} alt={astronaut} />
-
+    <img
+      className={style.not_found_page__img}
+      src={astronaut}
+      alt={astronaut}
+    />
   </div>
 );
