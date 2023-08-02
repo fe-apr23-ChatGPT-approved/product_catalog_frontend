6417cc55de05caeb560b3c3a1a8c73089fdbdb07
Yulia Shvets <juls.shvets@gmail.com>
2023-08-02 16:45:43 +0300

    add router

---
+++ src/components/Layout.tsx
@@ -0,0 +1,14 @@
+import {FC} from 'react';
+import { Outlet } from 'react-router-dom';
+import { Footer } from './Footer';
+// import { Header } from './Header';
+
+export const Layout: FC = () => (
+  <>
+    {/* <Header /> */}
+    <main>
+      <Outlet />
+    </main>
+    <Footer />
+  </>
+);
