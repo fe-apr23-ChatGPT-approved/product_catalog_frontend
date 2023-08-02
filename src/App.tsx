673f3a86d221bde97e20d16e1e516b487d25a3c3
Oleksandr Kosohor <kosogor2007@gmail.com>
2023-08-02 22:06:07 +0300

    add header + burger menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { Menu } from './components/Menu';
 
-export const App: React.FC = () => <Footer />;
+export const App: React.FC = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+  return (
+    <>
+      <Header isOpenMenu={isOpenMenu} setIsOpenMenu={setIsOpenMenu} />
+      
+      {isOpenMenu && <Menu setIsOpenMenu={setIsOpenMenu} />}
+      
+      {!isOpenMenu && <Footer />}
+    </>
+  );
+};
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import logo from '../../images/logo.svg';
+import cart from '../../images/Cart.svg';
+import close from '../../images/Close.svg';
+import like from '../../images/Like.svg';
+import burgerMenu from '../../images/Menu.svg';
+import { NavLink, Link } from 'react-router-dom';
+import cn from 'classnames';
+
+import style from './Header.module.scss';
+
+interface Props {
+  isOpenMenu: boolean;
+  setIsOpenMenu: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const Header: React.FC<Props> = ({ isOpenMenu, setIsOpenMenu }) => {
+  const pages = ['home', 'phones', 'tablets', 'accessories'];
+
+  const handleClick = () => {
+    setIsOpenMenu((prevState) => !prevState);
+  };
+
+  return (
+    <header className={style.header}>
+      <div className={style.header__content}>
+        <img className={style.header__main_logo} src={logo} alt={'main logo'} />
+        {!isOpenMenu && (
+          <ul className={style.header__list}>
+            {pages.map(page => (
+              <li className={style.header__item} key={page}>
+                <NavLink
+                  className={({ isActive }) => cn(`${style.header__link}`, {
+                    [style.header__link__is_active]: isActive,
+                  })}
+                  to={`/${page}`}
+                >
+                  {page}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div className={style.header__buttons}>
+        <Link className={`${style.header__button} ${style.header__button__tablet}`} to={'/like'}>
+          <img src={like} alt={'like button'} />
+        </Link>
+        <Link className={`${style.header__button} ${style.header__button__tablet}`} to={'cart'}>
+          <img src={cart} alt={'cart button'} />
+        </Link>
+        <div
+          className={`${style.header__button} ${style.header__button__mobile}`}
+          onClick={handleClick}
+        >
+          <img src={!isOpenMenu ? burgerMenu : close} alt={'menu button'} />
+        </div>
+      </div>
+    </header>
+  );
+};
