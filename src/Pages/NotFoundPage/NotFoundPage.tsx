c8d512ea360d22b71e383a5a5c33a4c1e41028b7
Roksolana Tylipska <roksolana1407@gmail.com>
2023-08-03 13:21:44 +0300

    fixed classNames

--- src/Pages/NotFoundPage/NotFoundPage.tsx
+++ src/Pages/NotFoundPage/NotFoundPage.tsx
@@ -3,21 +3,21 @@ import style from './NotFoundPage.module.scss';
 import astronaut from '../../images/page-not-found-astronaut.png';
 
 export const NotFoundPage: React.FC = () => (
-  <div className={style.not_found_page}>
-    <h1 className={style.not_found_page__title}>404</h1>
+  <div className={style['not-found-page']}>
+    <h1 className={style['not-found-page__title']}>404</h1>
 
-    <h3 className={style.not_found_page__text}>
+    <h3 className={style['not-found-page__text']}>
       LOST IN
-      <span className={style.not_found_page__text__crossedOut}> SPACE </span>
+      <span className={style['not-found-page__text--crossedOut']}> SPACE </span>
       App-Name? Hmm, looks like this page is not found.
     </h3>
 
     <a href="#">
-      <button className={style.not_found_page__link}>Go Home</button>
+      <button className={style['not-found-page__link']}>Go Home</button>
     </a>
 
     <img
-      className={style.not_found_page__img}
+      className={style['not-found-page__img']}
       src={astronaut}
       alt={astronaut}
     />
