06e20cb67ce59d48fd5beaa15dd943c17d22ff88
Anna Holovko <anutkagolovko1472@gmail.com>
2023-08-04 18:16:49 +0300

    pulled branch for rebase

--- src/components/CatalogLayout/CatalogLayout.tsx
+++ src/components/CatalogLayout/CatalogLayout.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import cn from 'classnames';
 import { Pagination } from '../Pagination/Pagination';
 import { getLastIndex } from '../../helpers/getNumbersPagination';
