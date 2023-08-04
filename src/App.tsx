f0f13658a5f77c8e0d045dba3c62144ac7de0995
Anna Holovko <anutkagolovko1472@gmail.com>
2023-08-04 16:54:07 +0300

    added pagination, added sorting, added catalogLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.scss';
 import { Footer } from './components/Footer';
-import { PaginationLayout } from './components/Pagination/PaginationLayout';
 
-export const App: React.FC = () =>
+export const App: React.FC = () => (
   <>
-    <PaginationLayout />
     <Footer />
-  </>;
+  </>
+);
