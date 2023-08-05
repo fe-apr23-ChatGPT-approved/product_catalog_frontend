6e3d6ae12dffa84f86ed884b60b37ddc83fa2e1f
Roksolana Tylipska <Roksolana1407@gmail.com>
2023-08-06 02:13:17 +0300

    product-list

---
+++ src/components/ProductList/ProductList.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+import { Product } from '../../types/product';
+import style from './ProductList.module.scss';
+import { ProductCard } from '../ProductCard/ProductCard';
+
+interface Props {
+  products: Product[];
+}
+
+export const ProductList: FC<Props> = ({ products }) => (
+  <section className={style['product-list']}>
+    {/* {products.map((product) => (
+      <ProductCard key={product.id} product={product} />
+    ))} */}
+  </section>
+);
---
+++ src/types/product.ts
@@ -0,0 +1,14 @@
+export interface Product {
+  id: number;
+  category: string;
+  itemId: string;
+  name: string;
+  fullPrice: number;
+  price: number;
+  screen: number | string;
+  capacity: string;
+  color: string;
+  ram: string;
+  year: number;
+  image: string;
+}
