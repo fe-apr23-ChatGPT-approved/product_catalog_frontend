e27d4c04a3257f681a15e2d8b560dc220eb409ef
Anna Holovko <anutkagolovko1472@gmail.com>
2023-08-06 11:12:59 +0300

    added closeItem, implemeted cartPage

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import React, { FC, useState } from 'react';
 import { TotalCost } from '../../components/TotalCost';
 import style from './CartPage.module.scss';
 import { CartItem } from '../../components/CartItem';
