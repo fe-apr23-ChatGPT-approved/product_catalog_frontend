86e568dd86929dbb4c34b939ae1d4f42fc677f46
Yulia Shvets <juls.shvets@gmail.com>
2023-08-07 20:53:00 +0300

    add recommendedPhonesEndpoint

--- src/api/getProductsFromServer.tsx
+++ src/api/getProductsFromServer.tsx
@@ -1,13 +1,17 @@
 const BASE_URL = 'https://gadget-store-api.onrender.com';
 
 function request(url: string) {
-  return fetch(`${BASE_URL}${url}`).then((response) => {
-    if (!response.ok) {
-      throw new Error(`${response.status} - ${response.statusText}`);
-    }
+  return fetch(`${BASE_URL}${url}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
 
     return response.json();
   });
 }
 
-export const getFromServer = (url: string) => request(url);
+export const getProducts = (url: string) => request(url);
+
+export const getRecommended = (url: string) => getProducts(`${url}/recomended`);
+///products/:id/recomended
