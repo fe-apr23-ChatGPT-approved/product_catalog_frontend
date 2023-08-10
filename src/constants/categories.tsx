a733b9a59f4c65ee774ec63b95f3c9d78568654f
Yulia Shvets <juls.shvets@gmail.com>
2023-08-10 17:44:51 +0300

    fixes

---
+++ src/constants/categories.tsx
@@ -0,0 +1,27 @@
+import phonesImg from '../images/category-photo-phones.png';
+import tabletsImg from '../images/category-photo-tablets.png';
+import accessoriesImg from '../images/category-photo-accessories.png';
+
+export const categories = [
+  {
+    id: 1,
+    title: 'Mobile Phones',
+    image: phonesImg,
+    url: '/phones',
+    quantity: 0,
+  },
+  {
+    id: 2,
+    title: 'Tablets',
+    image: tabletsImg,
+    url: '/tablets',
+    quantity: 0,
+  },
+  {
+    id: 3,
+    title: 'Accessories',
+    image: accessoriesImg,
+    url: '/accessories',
+    quantity: 0,
+  },
+];
